04cfe49fc8f55ff9831a763f45c4bb76888cb723
nagasawalin <shiezhenlin@gmail.com>
2020-09-12 00:21:23 +0800

    completed project1A now but may need to correct

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,122 @@
+/**Implemented in circular way*/
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int nextfirst;
+    private int nextlast;
+
+
+    /**Create an empty array deque*/
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        nextfirst = 0;
+        nextlast = 1;
+        size = 0;
+    }
+
+    public int plusindex(int x){
+        return (x + 1) % items.length;
+    }
+
+    public int minusindex(int x){
+        return (x - 1 + items.length) % items.length;
+    }
+
+    /**resize the array
+     * ! :新数组的nextfirst为新数组的最后一个也就是capacity-1;
+     * ! :新数组的nextlast为旧数组的size的值，因为旧数组到size-1就不够了*/
+    private void resize(int capacity) {
+        T[] newitems = (T[]) new Object[capacity];
+        /**Line31: the index of the first item in original deque
+         * since "nextfirst" is the one ahead of the first item, so we plusindex*/
+        int oldindex = plusindex(nextfirst);
+        for (int i = 0; i < size; i++){
+            newitems[i] = items[oldindex];
+            oldindex = plusindex(oldindex);
+        }
+        items = newitems;
+        /**The last one of the new array, also the former one ahead of 0 index*/
+        nextfirst = capacity - 1;
+        nextlast = size;
+    }
+
+    public void addFirst(T item){
+        if (size == items.length){
+            resize(size * 2);
+        }
+        items[nextfirst] = item;
+        nextfirst = minusindex(nextfirst);
+        size += 1;
+    }
+
+
+    public void addLast(T item){
+        if (size == items.length){
+            resize(size * 2);
+        }
+        items[nextlast] = item;
+        nextlast = plusindex(nextlast);
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+
+    public int size(){
+        return size;
+    }
+
+    /**prints the items in the deque from first to last,
+     * separated by a space*/
+    public void printDeque(){
+        int i = plusindex(nextfirst);
+        for (int j = 0; j < size; j++){
+            System.out.println(items[i]);
+            i = plusindex(i);
+        }
+    }
+
+    public T removeFirst(){
+        if (size == 0){
+            return null;
+        }
+        T a = items[plusindex(nextfirst)];
+        items[plusindex(nextfirst)] = null;
+        nextfirst = plusindex(nextfirst);
+        size -= 1;
+        if (items.length >= 16 && size < (items.length / 4)){
+            resize(items.length / 2);
+        }
+        return a;
+    }
+
+    public T removeLast(){
+        if (size == 0){
+            return null;
+        }
+        T a = items[minusindex(nextlast)];
+        items[minusindex(nextlast)] = null;
+        nextlast = minusindex(nextlast);
+        size -= 1;
+        if (items.length >= 16 && size < (items.length / 4)){
+            resize(items.length / 2);
+        }
+        return a;
+    }
+
+    public T get(int index){
+        if (index >= size){
+            return null;
+        }
+        int startingpoint = plusindex(nextfirst);
+        /**since the first one means the "0" algebra place of the array
+         * and we do not know where the array starts, maybe the middle or front or end
+         * so if we want to get the 'index' item of the array
+         * we have to let index plus the first one*/
+        return items[(startingpoint + index) / items.length];
+    }
+
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,133 @@
+/**This LinkedListDeque is based on double ended queue,
+ * which accepts generic types.
+ * All the method should follow "Deque API" described in
+ * https://sp18.datastructur.es/materials/proj/proj1a/proj1a#the-deque-api
+ * The amount of memory that this program uses at any given time must be
+ * proportional to the number of items.*/
+
+/**I do this part based on the idea of circular sentinel approaches*/
+
+public class LinkedListDeque <T> {
+    private int size;
+
+    private class itemNode{
+        private T item;
+        private itemNode pre;
+        private itemNode next;
+
+        public itemNode(T item, itemNode pre, itemNode next){
+            this.item = item;
+            this.pre = pre;
+            this.next = next;
+        }
+    }
+    /**why sentinel should be outside the itemNode Class???*/
+    private itemNode sentinel;
+
+
+    /**create an empty List and a sentinel node*/
+    public LinkedListDeque(){
+        sentinel = new itemNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.pre = sentinel;
+        size = 0;
+    }
+
+
+    /**Adds an item of type T to the front of the deque*/
+    public void addFirst(T item){
+        sentinel.next = new itemNode(item, sentinel, sentinel.next);
+        sentinel.next.next.pre = sentinel.next;
+        size += 1;
+    }
+
+    /**Adds an item of type T to the back of the deque*/
+    public void addLast(T item){
+        sentinel.pre = new itemNode(item, sentinel.pre, sentinel);
+        sentinel.pre.next = sentinel;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    /**Prints the items in the deque from first to last, separated by space*/
+    public void printDeque(){
+        itemNode p = sentinel.next;
+        while(p != null && p!= sentinel){
+            System.out.println(p.item + " ");
+            p = p.next;
+        }
+    }
+
+    /**Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null*/
+    public T removeLast(){
+        if (sentinel.next == sentinel){
+            return null;
+        }
+        T a = sentinel.pre.item;
+        sentinel.pre.pre.next = sentinel;
+        sentinel.pre = sentinel.pre.pre;
+        /**last line of code changes the arrow form the last node to sentinel node
+         * to the arrow from the second last node pointing to sentinel node;
+         * So the back and forth arrow between last and the second last node,
+         * only remains the one pointing from the last.pre to the second last node,
+         * we need to change this arrow in the next line*/
+        size --;
+        return a;
+    }
+
+    public T removeFirst(){
+        if (sentinel.next == sentinel){
+            return null;
+        }
+        T a = sentinel.next.item;
+        sentinel.next.next.pre = sentinel;
+        sentinel.next = sentinel.next.next;
+        size -= 1;
+        return a;
+        /*itemNode rFirst = sentinel.next;
+        rFirst.item = null;
+        rFirst.next.pre = sentinel;
+        sentinel.next = sentinel.next.next;
+        size -= 1;
+        T x = sentinel.next.item;
+        return x;*/
+    }
+
+    public T get(int index){
+        itemNode p = sentinel.next;
+        if (index > size - 1){
+            return null;
+        }
+        for (int i = 0; i < size; i++){
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    /**getRecursive方法无法递归
+     * 因为没有直接访问itemNode,也就是所传参数没有itemNode
+     * 不能产生对链表中节点的依次寻址*/
+    public T getRecursive(int index){
+        itemNode p = sentinel.next;
+        if(index > size){
+            return null;
+        }
+        return getRecursiveHelper(sentinel.next, index);
+
+    }
+
+    private T getRecursiveHelper(itemNode p, int i){
+        if (i == 0){
+            return p.item;
+        }
+        return getRecursiveHelper(p.next, i--);
+    }
+}
